5facea8548712e3c7ef1243c562853c64ff97cdb
David Molinari <contact@davidmolinari.fr>
2016-03-07 15:45:50 +0100

    Lancement

---
+++ CoursePooHeritage/CoursePooHeritage/Courseù.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursePooHeritage
+{
+    class Course<T>
+    {
+        private string nomCompetition;
+        private DateTime dateCompetition;
+
+        protected List<object> lesParticipants;
+
+        /// <summary>
+        ///  Procèdure qui ordonne la collection lesParticipants dans l'ordre de classement WCSL
+        ///  TODO
+        /// </summary>
+        protected void classerLesParticipants(){}
+    }
+}
